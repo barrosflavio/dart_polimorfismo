78ac654e6d6ee5f0365a261dc27d3cb8297597cf
barrosflavio <flavi011@outlook.com>
2024-06-04 16:18:59 -0300

    first commit

---
+++ test/polimorfismo_e_classes_abstratas_test.dart
@@ -0,0 +1,8 @@
+import 'package:polimorfismo_e_classes_abstratas/polimorfismo_e_classes_abstratas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
